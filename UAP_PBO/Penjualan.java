049a7373f7764941b2cb7db66e590258f2290d6c
Muhammad Fadhilah Ramadhani <92555706+00MFRamadhani00@users.noreply.github.com>
2022-12-18 07:04:17 +0700

    Update Penjualan.java

--- UAP_PBO/Penjualan.java
+++ UAP_PBO/Penjualan.java
@@ -16,11 +16,10 @@ public class Penjualan implements ProductCounter{
     private int jumlahProduk;
     private int stok;
 
-    public Penjualan(int id_penjualan, ArrayList<Produk> listProduk, int jumlahProduk, int stok) {
+    public Penjualan(int id_penjualan, int jumlahProduk, int stok) {
         this.id_penjualan = id_penjualan;
         this.jumlahProduk = jumlahProduk;
         this.stok = stok;
-        this.listProduk = new ArrayList<>();
     }    
 
     public int getId_penjualan() {
