91a0596c4af92d5ac6d93b6c57d343652f90d66d
Muhammad Fadhilah Ramadhani <92555706+00MFRamadhani00@users.noreply.github.com>
2022-12-18 07:01:18 +0700

    Update PenjualanModel.java

--- UAP_PBO/PenjualanModel.java
+++ UAP_PBO/PenjualanModel.java
@@ -23,4 +23,70 @@ public class PenjualanModel {
     public PenjualanModel() {
         this.CONN = DatabaseHelper.getConnection();
     }
+    
+    public void addPenjualan(Penjualan pjl, Produk prd){
+        String insert = "INSERT INTO penjualan (id_penjualan, id_produk, jumlah_produk, stok) VALUES (" 
+                +pjl.getId_penjualan()+ ",'" +pjl.getProduk(prd)+ "'," +pjl.getJumlahProduk()+ "," +pjl.getStok()+ ")" ;
+        
+        System.out.println(insert);
+        
+        try {
+            if(CONN.createStatement().executeUpdate(insert) > 0){
+                System.out.println("Berhasil Memasukkan Data");
+            }else{
+                System.out.println("Gagal Memasukkan Data");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PenjualanModel.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Gagal Memasukkan Data");
+        }
+    }
+    
+    public void delPenjualan(Penjualan pjl){
+        String delete = "DELETE FROM penjualan WHERE id_penjualan= " + pjl.getId_penjualan();
+        
+        System.out.println(delete);
+        try {
+            PreparedStatement dlt = CONN.prepareStatement(delete);
+            dlt.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(PenjualanModel.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public ArrayList<Produk> getPenjualan(){
+        String query = "SELECT * FROM penjualan";
+        ArrayList<Produk> pjl = new ArrayList<>();
+        
+        System.out.println(query);
+        try {
+            ResultSet rs = CONN.createStatement().executeQuery(query);
+            while(rs.next()){
+                Produk temp = new Produk(rs.getInt("id_produk"), rs.getString("nama_produk"), 
+                        rs.getDouble("harga"), rs.getInt("jumlah"), rs.getDouble("diskon"));
+                pjl.add(temp);
+            }
+            System.out.println("Berhasil Mengambil Data");
+        } catch (SQLException ex) {
+            Logger.getLogger(PenjualanModel.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Gagal Mengambil Data");
+        }
+        return pjl;
+    }
+    
+    public void upPenjualan(Penjualan pjl, Produk prd){
+        String update = "UPDATE penjualan SET id_produk='" +prd.getId_produk()+ "', jumlah= " 
+                +pjl.getJumlahProduk()+ ", stok= " +pjl.getStok()+
+                " WHERE id_penjualan= " +pjl.getId_penjualan();
+
+        System.out.println(update);
+        try {
+            PreparedStatement upd = CONN.prepareStatement(update);
+            upd.executeUpdate();
+            System.out.println("Berhasil Update Data");
+        } catch (SQLException ex) {
+            Logger.getLogger(PenjualanModel.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Gagal Update Data");
+        }
+    }
 }
