aa76ab1fa7a76cb0bffb87bb3a15704aafaaec51
Chandra Prasetya Putra <93764458+chandpras@users.noreply.github.com>
2022-12-18 15:44:08 +0700

    Update UAP_pbo.java

--- UAP_PBO/FXML/UAP_pbo.java
+++ UAP_PBO/FXML/UAP_pbo.java
@@ -1,19 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
- */
-package uap_pbo;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Acer
- */
 public class UAP_pbo extends Application {
     
     @Override
